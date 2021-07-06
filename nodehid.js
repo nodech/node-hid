4f2954f9fb07401bfc88ba1c622c560137feb572
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2021-07-06 17:18:58 +0400

    pkg: replace binding with loady and use forked node-addon-api.

--- nodehid.js
+++ nodehid.js
@@ -16,13 +16,13 @@ function loadBinding() {
         if( os.platform() === 'linux' ) {
             // Linux defaults to hidraw
             if( !driverType || driverType === 'hidraw' ) {
-                binding = require('bindings')('HID_hidraw.node');
+                binding = require('loady')('HID_hidraw.node', __dirname);
             } else {
-                binding = require('bindings')('HID.node');
+                binding = require('loady')('HID.node', __dirname);
             }
         }
         else {
-            binding = require('bindings')('HID.node');
+            binding = require('loady')('HID.node', __dirname);
         }
     }
 }
